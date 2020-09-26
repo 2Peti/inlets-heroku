86f059b68080cc394bdc5fda8ed666503c284814
2Peti <pney0807@gmail.com>
2020-09-26 20:41:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('*', function(req,res,next) {
-  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-    res.redirect('https://'+req.hostname+req.url)
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    res.redirect('https://cochces.herokuapp.com'+req.url)
   else
     next() /* Continue to other routes if we're not redirecting */
-});
-
+})
 
 app.listen(port, function() {
 	console.log('app running')
