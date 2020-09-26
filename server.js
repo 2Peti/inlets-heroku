eb151bdd5d1096dd03e9ad44ae14bcecd0694556
2Peti <pney0807@gmail.com>
2020-09-26 20:54:09 +0200

    update

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-var express = require('express')
-var app = express();
-
-var port = process.env.PORT || 8080;
-
-app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto']!='https')
-    res.redirect('https://cochces.herokuapp.com'+req.url)
-  else
-    next() /* Continue to other routes if we're not redirecting */
-})
-
-app.listen(port, function() {
-	console.log('app running')
-})
-
+import sslRedirect from 'heroku-ssl-redirect';
+import express from 'express';
+const app = express();
+ 
+// enable ssl redirect
+app.use(sslRedirect());
+ 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 3000);
